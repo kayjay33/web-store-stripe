08c4061aa3ad8bd4d707c294057d469acb43aaf6
Kevin Hoffman Jr. <kevhoffmanj@gmail.com>
2023-10-14 09:44:16 -0400

    Adding finished header & starting body

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   // templateUrl: './app.component.html',
-  template: `<app-header></app-header>`,
+  template: `
+  <app-header></app-header>
+  <router-outlet></router-outlet>
+  `,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
