2848a74bdc37ce457b56df8d4776778cab9ae541
Kevin Hoffman Jr. <kevhoffmanj@gmail.com>
2023-10-19 19:53:15 -0400

    Finishing up server & stripe

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { loadStripe } from '@stripe/stripe-js';
 import { Cart, CartItem } from 'src/app/models/cart.model';
 import { CartService } from 'src/app/services/cart.service';
 
@@ -43,7 +45,7 @@ export class CartComponent implements OnInit {
     'action',
   ];
 
-  constructor(private cartService: CartService) {}
+  constructor(private cartService: CartService, private http: HttpClient) {}
 
   ngOnInit(): void {
     this.dataSource = this.cart.items;
@@ -71,5 +73,16 @@ export class CartComponent implements OnInit {
 
   onRemoveQuantity(item: CartItem): void {
     this.cartService.removeQuantity(item);
+  }
+
+  onCheckout(): void {
+    this.http.post('http://localhost:4242/checkout', {
+      items: this.cart.items
+    }).subscribe(async(res: any) => {
+      let stripe = await loadStripe('pk_test_51O35IcLRwvCJs9x1v3QErCKiFHcs5gY78iWHqvIyqjcWXVQ2LmxmfX8qO272RjVmaTAcFjRWM315LdLsft5gF2MJ00oDz684wf');
+      stripe?.redirectToCheckout({
+        sessionId: res.id
+      })
+    })
   }
 }
